770281c65559b5318f188960b4ed3aa5b8728328
alysachan <alysachansl@gmail.com>
2021-07-04 18:23:49 +0800

    Add top banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <Menu></Menu>
-      <h1 class="title">flash_ticketing</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="position-relative">
+    <div
+      class="
+        top-banner
+        bg-img
+        d-flex
+        justify-content-center
+        align-items-center
+        img-filter--dark
+      "
+    ></div>
+    <div class="position-absolute top-banner-title text-white text-center">
+      <h2 class="mb-8 font-2xl">亞洲年度新媒體藝術節 2021</h2>
+      <div class="mb-12 d-flex justify-content-center">
+        <p class="me-3">
+          <span class="material-icons align-text-top me-1 text-white font-base">
+            calendar_today
+          </span>
+          06-15 — 07-15
+        </p>
+        <p>
+          <span class="material-icons align-text-top me-1 text-white font-base">
+            location_on
+          </span>
+          國際展覽中心一號展場
+        </p>
       </div>
+      <button class="btn btn-primary btn-lg">搶先購票</button>
     </div>
   </div>
 </template>
 
 <script>
 // import axios from 'axios'
-import Menu from '@/components/Menu.vue'
 import { apiClientGetProduct } from '../api/index'
 export default {
-  components: {
-    Menu,
-  },
   async asyncData({ env }) {
     // try{
     //   const productRes = await axios.get(`${apiBaseUrl}/api/${apiPath}/products?page=1`);
@@ -103,35 +104,47 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style lang="scss" scoped>
+.top-banner {
+  background-image: url('@/assets/images/brandon-erlinger-ford-wI2Hafqr_f4-unsplash.jpg');
+  // background: center / cover no-repeat
+  //   url('@/assets/images/brandon-erlinger-ford-wI2Hafqr_f4-unsplash.jpg');
+  height: 388px;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.top-banner-title {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
+// .container {
+//   margin: 0 auto;
+//   min-height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   text-align: center;
+// }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+// .title {
+//   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+//     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+//   display: block;
+//   font-weight: 300;
+//   font-size: 100px;
+//   color: #35495e;
+//   letter-spacing: 1px;
+// }
 
-.links {
-  padding-top: 15px;
-}
+// .subtitle {
+//   font-weight: 300;
+//   font-size: 42px;
+//   color: #526488;
+//   word-spacing: 5px;
+//   padding-bottom: 15px;
+// }
+
+// .links {
+//   padding-top: 15px;
+// }
 </style>
