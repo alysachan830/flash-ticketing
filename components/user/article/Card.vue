7a565eb6f6dced2a889784f4379a7f6da387ebc2
alysachan <alysachansl@gmail.com>
2021-07-05 04:26:53 +0800

    Add article card

---
+++ components/user/article/Card.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-7">
+          <div
+            :style="`background-image:url(${img1})`"
+            class="article-img rounded-start"
+          ></div>
+          <!-- <div :style="`background-image: ${img1}`" class="article-img"></div> -->
+          <!-- <img
+            src="@/assets/images/jurien-huggins-drIebcFZqpE-unsplash.jpg"
+            class="img-fluid rounded-start"
+            alt="article image"
+            style="height: 352px"
+          /> -->
+        </div>
+        <div class="col-md-5">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      img1: `https://storage.googleapis.com/vue-course-api.appspot.com/flashticketing/1625410413238.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=bQn0N7fWYIW9PUueEzSdl02l5G9AWZrEFQ4KDTMElcBv2t%2F4TqPZcOBkYhHtlWEKPVNLE%2FXbFi4%2F39kHiqpbjDbvYGAEfRw34P7wbkLGM20xE%2Bgk8feMKkMvJJVU0dV1cNLTAvKRjQO3Pa44EELy9lYocbOpWDk8X95o5WxsXk%2B1pEFrAVzEAKVRGTVcVHTnPdtvGBACe0wLtY4NBq2oGDTWsDYgSKLxVSgJ5UeVqhiA8aDyYl2x%2Bk85hu0grujfexbY7l8Ch9GXLladfkN1BV67ozs9ETbnhfj%2FXLtylUdWs0SMVHcBIg9utq0Ogcj4yGNCRM0Dr8t3wD5pYhYTXg%3D%3D`,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border-radius: 10px;
+}
+
+.article-img {
+  height: 352px;
+  background-size: cover;
+}
+
+.rounded-start {
+  border-bottom-left-radius: 10px !important;
+  border-top-left-radius: 10px !important;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
             >
               calendar_today
             </span>
-            06-15 — 07-15
+            2021-06-15 — 2021-07-15
           </p>
           <p>
             <span
@@ -110,45 +110,129 @@
     <!-- Body -->
     <div class="container">
       <!-- Hot events -->
-      <div class="mb-22">
-        <div class="mb-14">
-          <h2 class="font-xl mb-4">熱門節目</h2>
-          <p>時刻緊貼潮流，為您帶來最豐富的節目體驗。</p>
+      <div class="mb-23">
+        <div class="mb-22">
+          <div class="mb-14">
+            <h2 class="font-xl mb-4">熱門節目</h2>
+            <p>時刻緊貼潮流，為您帶來最豐富的節目體驗。</p>
+          </div>
+          <SwiperHotEvents :hot-events="hotEvents"></SwiperHotEvents>
         </div>
-        <SwiperHotEvents :hot-events="hotEvents"></SwiperHotEvents>
-      </div>
-      <!-- Hot events cards -->
-      <div class="row">
-        <!-- <div v-for="n in 8" :key="n" class="col-3"> -->
-        <div v-for="n in 8" :key="n" class="col-3">
-          <NuxtLink to="/" class="card border-0 mb-10">
-            <img
-              src="https://storage.googleapis.com/vue-course-api.appspot.com/flashticketing/1625172152483.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=A6g2dOim3jb8EcodT4ceEsWkJ70%2FxnSUcM9kBmsVYu6D7N4yJ2gRrmnuD6qzBA4N7CvKczPW5%2FXRQ4R%2FobN5EKw9bLMY7cLEFvQ0EC1bmi%2Bsopo%2FuQj5PeflPgo3DudOUV4qyAl4d3y4J5fVJQJzTPwP70L4vp094v8A%2BbRtgNQ15LpPo2%2FMK39FQcgZEgkJej1BYd4syzhcdsP3Oftq45wfGw27LlOgjEQmUAvTQVj%2B9cPE43LAUGRGL4sHU%2Fi8Iecx7sSM0n6J6%2B7tXx3%2Fcn1BWKE%2Bf2lY5eBIt4Ln1JyN3rFvCEbmMGRcTHvZrBKCLnIAHAeTxE3UEKStXr8p3A%3D%3D"
-              class="card-img-top hover__filter--darken rounded-4 mb-7"
-              alt="hot event"
-            />
-            <span class="card-body d-block">
-              <div class="d-flex justify-content-between">
-                <div>
-                  <h5 class="card-title">亞洲年度新媒體藝術節 2021</h5>
-                  <div class="mb-6">
-                    <span class="font-base material-icons align-text-top me-2">
-                      calendar_today </span
-                    >2021.06.08 — 2021.11.09
+        <!-- Hot events cards -->
+        <div class="row">
+          <div v-for="n in 8" :key="n" class="col-3">
+            <NuxtLink to="/" class="card border-0 mb-10">
+              <img
+                src="https://storage.googleapis.com/vue-course-api.appspot.com/flashticketing/1625172152483.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=A6g2dOim3jb8EcodT4ceEsWkJ70%2FxnSUcM9kBmsVYu6D7N4yJ2gRrmnuD6qzBA4N7CvKczPW5%2FXRQ4R%2FobN5EKw9bLMY7cLEFvQ0EC1bmi%2Bsopo%2FuQj5PeflPgo3DudOUV4qyAl4d3y4J5fVJQJzTPwP70L4vp094v8A%2BbRtgNQ15LpPo2%2FMK39FQcgZEgkJej1BYd4syzhcdsP3Oftq45wfGw27LlOgjEQmUAvTQVj%2B9cPE43LAUGRGL4sHU%2Fi8Iecx7sSM0n6J6%2B7tXx3%2Fcn1BWKE%2Bf2lY5eBIt4Ln1JyN3rFvCEbmMGRcTHvZrBKCLnIAHAeTxE3UEKStXr8p3A%3D%3D"
+                class="card-img-top hover__filter--darken rounded-4 mb-7"
+                alt="hot event"
+              />
+              <span class="card-body d-block">
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <h5 class="card-title">亞洲年度新媒體藝術節 2021</h5>
+                    <div class="mb-6">
+                      <span
+                        class="font-base material-icons align-text-top me-2"
+                      >
+                        calendar_today </span
+                      >2021.06.08 — 2021.11.09
+                    </div>
                   </div>
+                  <a href="#"
+                    ><span class="material-icons"> bookmark_border </span></a
+                  >
                 </div>
-                <a href="#"
-                  ><span class="material-icons"> bookmark_border </span></a
-                >
-              </div>
-              <span class="badge font-s bg-secondary text-black mb-4">
-                藝術展覽
+                <span class="badge font-s bg-secondary text-black mb-4">
+                  藝術展覽
+                </span>
               </span>
-            </span>
-          </NuxtLink>
+            </NuxtLink>
+          </div>
         </div>
-        <!-- </div> -->
+        <div class="text-center">
+          <button class="btn btn-lg btn-outline-primary px-16">更多節目</button>
+        </div>
       </div>
+    </div>
+    <!-- New events -->
+    <div class="mb-23">
+      <!-- New event banner -->
+      <a href="#" class="position-relative mb-19">
+        <span class="d-block">
+          <div class="latest-event-banner bg-img banner-filter"></div>
+          <div class="position-absolute latest-event-title">
+            <h2 class="mb-8 font-2xl text-white">
+              夏日狂想曲 —— 莫扎特的交響樂
+            </h2>
+            <div class="mb-12">
+              <p class="me-3 text-white font-l">
+                <span class="material-icons align-text-top me-1 font-l me-3">
+                  calendar_today
+                </span>
+                2021-09-15
+              </p>
+              <p class="text-white font-l">
+                <span class="material-icons align-text-top me-1 font-l me-3">
+                  location_on
+                </span>
+                文化與藝術協會中心音樂廳
+              </p>
+            </div>
+            <button class="btn btn-primary btn-lg px-10">搶先購票</button>
+          </div>
+        </span>
+      </a>
+      <!-- New event title -->
+      <div class="mb-14 container">
+        <h2 class="font-xl mb-4">最新節目</h2>
+        <p>搶先體驗最新節目活動。</p>
+      </div>
+      <!-- New events cards -->
+      <div class="container">
+        <div class="row">
+          <div v-for="n in 8" :key="n" class="col-3">
+            <NuxtLink to="/" class="card border-0 mb-10">
+              <img
+                src="https://storage.googleapis.com/vue-course-api.appspot.com/flashticketing/1625172152483.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=A6g2dOim3jb8EcodT4ceEsWkJ70%2FxnSUcM9kBmsVYu6D7N4yJ2gRrmnuD6qzBA4N7CvKczPW5%2FXRQ4R%2FobN5EKw9bLMY7cLEFvQ0EC1bmi%2Bsopo%2FuQj5PeflPgo3DudOUV4qyAl4d3y4J5fVJQJzTPwP70L4vp094v8A%2BbRtgNQ15LpPo2%2FMK39FQcgZEgkJej1BYd4syzhcdsP3Oftq45wfGw27LlOgjEQmUAvTQVj%2B9cPE43LAUGRGL4sHU%2Fi8Iecx7sSM0n6J6%2B7tXx3%2Fcn1BWKE%2Bf2lY5eBIt4Ln1JyN3rFvCEbmMGRcTHvZrBKCLnIAHAeTxE3UEKStXr8p3A%3D%3D"
+                class="card-img-top hover__filter--darken rounded-4 mb-7"
+                alt="hot event"
+              />
+              <span class="card-body d-block">
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <h5 class="card-title">亞洲年度新媒體藝術節 2021</h5>
+                    <div class="mb-6">
+                      <span
+                        class="font-base material-icons align-text-top me-2"
+                      >
+                        calendar_today </span
+                      >2021.06.08 — 2021.11.09
+                    </div>
+                  </div>
+                  <a href="#"
+                    ><span class="material-icons"> bookmark_border </span></a
+                  >
+                </div>
+                <span class="badge font-s bg-secondary text-black mb-4">
+                  藝術展覽
+                </span>
+              </span>
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="text-center">
+          <button class="btn btn-lg btn-outline-primary px-16">更多節目</button>
+        </div>
+      </div>
+    </div>
+    <!-- Recommendation -->
+    <div class="mb-14 container">
+      <div class="mb-19">
+        <h2 class="font-xl mb-4">獨家推薦</h2>
+        <p>各界知名藝評人為您推薦最高質的節目。</p>
+      </div>
+      <articleCard></articleCard>
     </div>
   </div>
 </template>
@@ -222,5 +306,17 @@ export default {
   &-body {
     padding: 0 4px;
   }
+}
+
+.latest-event-banner {
+  background-image: url('@/assets/images/arindam-mahanta-VEOk8qUl9DU-unsplash.jpg');
+  background-position: bottom;
+  height: 720px;
+}
+
+.latest-event-title {
+  left: 118px;
+  top: 50%;
+  transform: translateY(-50%);
 }
 </style>
