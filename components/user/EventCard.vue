edacf3f60915bd5e8bc5e552f66f794464a4a196
alysachan <alysachansl@gmail.com>
2021-07-06 02:13:00 +0800

    Create event card component

---
+++ components/user/EventCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <NuxtLink to="/" class="card border-0 mb-10">
+    <img
+      src="https://storage.googleapis.com/vue-course-api.appspot.com/flashticketing/1625172152483.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=A6g2dOim3jb8EcodT4ceEsWkJ70%2FxnSUcM9kBmsVYu6D7N4yJ2gRrmnuD6qzBA4N7CvKczPW5%2FXRQ4R%2FobN5EKw9bLMY7cLEFvQ0EC1bmi%2Bsopo%2FuQj5PeflPgo3DudOUV4qyAl4d3y4J5fVJQJzTPwP70L4vp094v8A%2BbRtgNQ15LpPo2%2FMK39FQcgZEgkJej1BYd4syzhcdsP3Oftq45wfGw27LlOgjEQmUAvTQVj%2B9cPE43LAUGRGL4sHU%2Fi8Iecx7sSM0n6J6%2B7tXx3%2Fcn1BWKE%2Bf2lY5eBIt4Ln1JyN3rFvCEbmMGRcTHvZrBKCLnIAHAeTxE3UEKStXr8p3A%3D%3D"
+      class="card-img-top hover__filter--darken rounded-4 mb-7"
+      alt="hot event"
+    />
+    <span class="card-body d-block">
+      <div class="d-flex justify-content-between">
+        <div>
+          <h5 class="card-title">亞洲年度新媒體藝術節 2021</h5>
+          <div class="mb-6">
+            <span class="font-base material-icons align-text-top me-2">
+              calendar_today </span
+            >2021.06.08 — 2021.11.09
+          </div>
+        </div>
+        <a href="#"><span class="material-icons"> bookmark_border </span></a>
+      </div>
+      <span class="badge font-s bg-secondary text-black mb-4"> 藝術展覽 </span>
+    </span>
+  </NuxtLink>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/stylesheets/all';
+
+.card {
+  &-img-top {
+    min-height: calc(
+      #{map-get($container-max-widths, 'xxl')} / 4 - #{$grid-gutter-width} * 3
+    );
+  }
+  &-body {
+    padding: 0 4px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -121,6 +121,11 @@
         <!-- Hot events cards -->
         <div class="row">
           <div v-for="n in 8" :key="n" class="col-3">
+            <EventCard></EventCard>
+          </div>
+        </div>
+        <!-- <div class="row">
+          <div v-for="n in 8" :key="n" class="col-3">
             <NuxtLink to="/" class="card border-0 mb-10">
               <img
                 src="https://storage.googleapis.com/vue-course-api.appspot.com/flashticketing/1625172152483.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=A6g2dOim3jb8EcodT4ceEsWkJ70%2FxnSUcM9kBmsVYu6D7N4yJ2gRrmnuD6qzBA4N7CvKczPW5%2FXRQ4R%2FobN5EKw9bLMY7cLEFvQ0EC1bmi%2Bsopo%2FuQj5PeflPgo3DudOUV4qyAl4d3y4J5fVJQJzTPwP70L4vp094v8A%2BbRtgNQ15LpPo2%2FMK39FQcgZEgkJej1BYd4syzhcdsP3Oftq45wfGw27LlOgjEQmUAvTQVj%2B9cPE43LAUGRGL4sHU%2Fi8Iecx7sSM0n6J6%2B7tXx3%2Fcn1BWKE%2Bf2lY5eBIt4Ln1JyN3rFvCEbmMGRcTHvZrBKCLnIAHAeTxE3UEKStXr8p3A%3D%3D"
@@ -149,7 +154,7 @@
               </span>
             </NuxtLink>
           </div>
-        </div>
+        </div> -->
         <div class="text-center">
           <button class="btn btn-lg btn-outline-primary px-16">更多節目</button>
         </div>
@@ -192,33 +197,7 @@
       <div class="container">
         <div class="row">
           <div v-for="n in 8" :key="n" class="col-3">
-            <NuxtLink to="/" class="card border-0 mb-10">
-              <img
-                src="https://storage.googleapis.com/vue-course-api.appspot.com/flashticketing/1625172152483.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=A6g2dOim3jb8EcodT4ceEsWkJ70%2FxnSUcM9kBmsVYu6D7N4yJ2gRrmnuD6qzBA4N7CvKczPW5%2FXRQ4R%2FobN5EKw9bLMY7cLEFvQ0EC1bmi%2Bsopo%2FuQj5PeflPgo3DudOUV4qyAl4d3y4J5fVJQJzTPwP70L4vp094v8A%2BbRtgNQ15LpPo2%2FMK39FQcgZEgkJej1BYd4syzhcdsP3Oftq45wfGw27LlOgjEQmUAvTQVj%2B9cPE43LAUGRGL4sHU%2Fi8Iecx7sSM0n6J6%2B7tXx3%2Fcn1BWKE%2Bf2lY5eBIt4Ln1JyN3rFvCEbmMGRcTHvZrBKCLnIAHAeTxE3UEKStXr8p3A%3D%3D"
-                class="card-img-top hover__filter--darken rounded-4 mb-7"
-                alt="hot event"
-              />
-              <span class="card-body d-block">
-                <div class="d-flex justify-content-between">
-                  <div>
-                    <h5 class="card-title">亞洲年度新媒體藝術節 2021</h5>
-                    <div class="mb-6">
-                      <span
-                        class="font-base material-icons align-text-top me-2"
-                      >
-                        calendar_today </span
-                      >2021.06.08 — 2021.11.09
-                    </div>
-                  </div>
-                  <a href="#"
-                    ><span class="material-icons"> bookmark_border </span></a
-                  >
-                </div>
-                <span class="badge font-s bg-secondary text-black mb-4">
-                  藝術展覽
-                </span>
-              </span>
-            </NuxtLink>
+            <EventCard></EventCard>
           </div>
         </div>
         <div class="text-center">
@@ -232,7 +211,7 @@
         <h2 class="font-xl mb-4">獨家推薦</h2>
         <p>各界知名藝評人為您推薦最高質的節目。</p>
       </div>
-      <articleCard class="mb-10" v-for="n in 4" :key="n"></articleCard>
+      <articleCard v-for="n in 4" :key="n" class="mb-10"></articleCard>
       <div class="text-center mt-8">
         <button class="btn btn-lg btn-outline-primary px-16">更多評論</button>
       </div>
@@ -304,8 +283,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/stylesheets/all';
-
 .top-banner {
   background-image: url('@/assets/images/brandon-erlinger-ford-wI2Hafqr_f4-unsplash.jpg');
   // background: center / cover no-repeat
@@ -329,16 +306,16 @@ export default {
   }
 }
 
-.card {
-  &-img-top {
-    min-height: calc(
-      #{map-get($container-max-widths, 'xxl')} / 4 - #{$grid-gutter-width} * 3
-    );
-  }
-  &-body {
-    padding: 0 4px;
-  }
-}
+// .card {
+//   &-img-top {
+//     min-height: calc(
+//       #{map-get($container-max-widths, 'xxl')} / 4 - #{$grid-gutter-width} * 3
+//     );
+//   }
+//   &-body {
+//     padding: 0 4px;
+//   }
+// }
 
 .latest-event-banner {
   background-image: url('@/assets/images/arindam-mahanta-VEOk8qUl9DU-unsplash.jpg');
