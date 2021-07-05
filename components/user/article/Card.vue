0ada6b317c44677d3abb6aebdd9227163708e5b7
alysachan <alysachansl@gmail.com>
2021-07-05 23:02:28 +0800

    Modify article card style

--- components/user/article/Card.vue
+++ components/user/article/Card.vue
@@ -1,36 +1,84 @@
 <template>
-  <div>
-    <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-7">
-          <div
-            :style="`background-image:url(${img1})`"
-            class="article-img rounded-start"
-          ></div>
-          <!-- <div :style="`background-image: ${img1}`" class="article-img"></div> -->
-          <!-- <img
-            src="@/assets/images/jurien-huggins-drIebcFZqpE-unsplash.jpg"
-            class="img-fluid rounded-start"
-            alt="article image"
-            style="height: 352px"
-          /> -->
-        </div>
-        <div class="col-md-5">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p class="card-text">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </p>
+  <a class="article-link" href="#">
+    <span class="d-block">
+      <div class="card mb-3 overflow-hidden">
+        <div class="row g-0">
+          <div class="col-lg-8 col-md-5 position-relative">
+            <div
+              :style="`background-image:url(${img1})`"
+              class="article-img rounded-start"
+            ></div>
+            <div
+              class="
+                article-info
+                position-absolute
+                text-white
+                d-flex
+                flex-column
+                justify-content-between
+                h-100
+                p-12 p-xl-15
+                w-100
+              "
+            >
+              <div>
+                <span class="d-block mb-4 font-xs font-md-s">推介指數</span>
+                <span class="font-xl-4xl font-3xl lh-1">4.5</span>
+                <div>
+                  <span class="material-icons font-base"> star </span>
+                  <span class="material-icons font-base"> star </span>
+                  <span class="material-icons font-base"> star </span>
+                  <span class="material-icons font-base"> star </span>
+                </div>
+              </div>
+              <div>
+                <span class="badge font-s bg-secondary text-black mb-4">
+                  藝術展覽
+                </span>
+                <h2 class="font-xl-xl font-md-l lh-base">
+                  VR x 數碼龐克展 — 科幻世界與藝術表現
+                </h2>
+              </div>
+            </div>
+            <!-- <div :style="`background-image: ${img1}`" class="article-img"></div> -->
+            <!-- <img
+              src="@/assets/images/jurien-huggins-drIebcFZqpE-unsplash.jpg"
+              class="img-fluid rounded-start"
+              alt="article image"
+              style="height: 352px"
+            /> -->
+          </div>
+          <div class="col-lg-4 col-md-7 p-md-13 p-8">
+            <div
+              class="card-body d-flex flex-column justify-content-between h-100"
+            >
+              <div class="mb-5 mb-md-0">
+                <!-- <h5 class="card-title">Card title</h5> -->
+                <p class="card-text mb-md-3 font-xxl-base font-s lh-base">
+                  不以招地源天公；的又型、衣位中他名且分系象展不價、式看舉兒離時操、說持於公、中後以海：有見海感。思進說話怎子沒！如野果動眾。年建變如我深個千維支沒配大表起心多就長明致且數球感住有點草或家，完說長？認日用不起們傳計我停時火月...
+                </p>
+                <p class="card-text">
+                  <small class="text-muted font-md-s font-xs"
+                    >發佈日期：06-06-2021</small
+                  >
+                </p>
+              </div>
+              <div class="d-flex justify-content-between align-items-center">
+                <button class="btn btn-outline-primary font-s font-md-base">
+                  閱讀更多
+                </button>
+                <a href="#" class="text-primary font-s font-md-base"
+                  >查看節目<span class="material-icons">
+                    chevron_right
+                  </span></a
+                >
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
+    </span>
+  </a>
 </template>
 
 <script>
@@ -44,17 +92,53 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/stylesheets/all';
+
+a:hover {
+  color: inherit;
+}
+
 .card {
   border-radius: 10px;
+
+  &-body {
+    padding: 0;
+  }
+}
+
+.article-link {
+  transition: filter 0.3s;
+  &:hover {
+    filter: drop-shadow(0.5px 0.5px 3px #e0e0e0);
+  }
 }
 
 .article-img {
   height: 352px;
   background-size: cover;
+  filter: brightness(50%);
+  // transition: transform 0.3s;
+  // &:hover {
+  //   transform: scale(1.1);
+  // }
+}
+
+.article-info {
+  top: 0;
+  left: 0;
+  // transition: background 0.3s;
+  // &:hover {
+  //   // transform: scale(1.1);
+  //   background: rgb($primary, 0.2);
+  // }
 }
 
 .rounded-start {
-  border-bottom-left-radius: 10px !important;
+  border-bottom-left-radius: 0 !important;
   border-top-left-radius: 10px !important;
+
+  @include media-breakpoint-up(md) {
+    border-bottom-left-radius: 10px !important;
+  }
 }
 </style>
