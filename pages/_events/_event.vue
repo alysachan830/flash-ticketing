eceee8eb9370c93a14ece69a8757883953aa5d38
alysachan <alysachansl@gmail.com>
2021-07-07 11:29:04 +0800

    Add event route

---
+++ pages/_events/_event.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ event }}</div>
+</template>
+
+<script>
+export default {
+  asyncData({ params }) {
+    const event = params.event
+    return { event }
+  },
+}
+</script>
+
+<style></style>
