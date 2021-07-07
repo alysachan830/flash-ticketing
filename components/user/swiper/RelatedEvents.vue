3b1973b11d7b560ba0ca1c67f433e69be3b06207
alysachan <alysachansl@gmail.com>
2021-07-07 13:49:42 +0800

    Add event page layout

---
+++ components/user/swiper/RelatedEvents.vue
@@ -0,0 +1,117 @@
+<template>
+  <!-- Slider main container -->
+  <div class="swiper-container">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+      <!-- Slides -->
+      <div v-for="n in 8" :key="n" class="swiper-slide position-relative">
+        <EventCard class="bg-transparent"></EventCard>
+      </div>
+      <!-- <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div> -->
+    </div>
+    <!-- If we need pagination -->
+    <div class="swiper-pagination"></div>
+
+    <!-- If we need navigation buttons -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  </div>
+</template>
+
+<script>
+import Swiper, {
+  Autoplay,
+  Navigation,
+  Pagination,
+} from '@/node_modules/swiper/core'
+
+Swiper.use([Autoplay, Navigation, Pagination])
+
+export default {
+  //   props: {
+  //     hotEvents: {
+  //       type: Array,
+  //       default() {
+  //         return []
+  //       },
+  //       require: true,
+  //     },
+  //   },
+  data() {
+    return {
+      swiper: {},
+      img1: `background-image: url('https://storage.googleapis.com/vue-course-api.appspot.com/flashticketing/1625173434912.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=da8BPRMDF5JnemX4M5y5mBwKq1vDshpwDkoT01dU5RwXUTyMoacAlk5j9PH7O2kkGBJRVMFQFn0YlEs5NNIEB9zTiKgvz30%2BXJhY1M4PVNKrJcErhKNHUNhlKFijNOEVflBHAeTWvhjMWrQvCa11pdgvTpKzlqQqtdo5B4dUk0C4OL4tGG7ULJ%2FFJDjXCiR5DrpL7wgYvZUj6ByyTD1FLOfsQuBmS%2FpzAl9MuwgQKFaaLywSijBjjaIhieIJ%2Bv5CnJZPI0Iv8IIndeJAFzbxvsCsPclptlTe%2Fy5Hl%2F8vXVr6lM6yUcdGgSN%2B2i%2B14YE2Z4Sbh6mjE37XLmm2KC1%2BGA%3D%3D')`,
+    }
+  },
+  mounted() {
+    this.swiper = new Swiper('.swiper-container', {
+      observer: true,
+      observeParents: true,
+      // slidesPerView: 1.5,
+      // loop: true,
+      // loopedSlides: 8,
+      spaceBetween: 30,
+      // centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+
+        992: {
+          slidesPerView: 4,
+        },
+      },
+      // on: {
+      //   autoplay() {
+      //     vm.$bus.$emit("triggerAnimation");
+      //   },
+      // },
+      // onProgress: this.$bus.$emit("triggerAnimation"),
+      // onProgress: console.log("123"),
+    })
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/node_modules/swiper/swiper-bundle.css';
+
+.slide-img-bg {
+  //   height: 382px;
+  background-size: cover;
+  background-position: center;
+  filter: brightness(75%);
+}
+
+.slide-title-wrap {
+  left: 40px;
+  top: 40px;
+}
+
+.swiper-button-next,
+.swiper-button-prev {
+  color: #fff;
+  font-size: 20px;
+}
+
+.swiper-button-next {
+  margin-right: 20px;
+}
+
+.swiper-button-prev {
+  margin-left: 20px;
+}
+</style>
--- pages/_events/_event.vue
+++ pages/_events/_event.vue
@@ -1,5 +1,123 @@
 <template>
-  <div>{{ event }}</div>
+  <div class="pt-23">
+    <div class="container mb-23">
+      <!-- Event information -->
+      <div class="mb-23">
+        <div class="row">
+          <div class="col-6">
+            <div
+              class="main-image bg-img rounded-4 mb-7"
+              :style="`background-image: url('/_nuxt/assets/images/hudson-hintze-pzgn0feLJwg-unsplash.jpg');`"
+            ></div>
+            <div class="row">
+              <div class="col-6">
+                <div
+                  class="sub-image bg-img rounded-4 mb-7"
+                  :style="`background-image: url('/_nuxt/assets/images/abhyuday-majhi-bW-vRGOF5EI-unsplash.jpg');`"
+                ></div>
+              </div>
+              <div class="col-6">
+                <div
+                  class="sub-image bg-img rounded-4 mb-7"
+                  :style="`background-image: url('/_nuxt/assets/images/c-michael-afonso-nZU76qWy-T8-unsplash.jpg');`"
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-5 offset-1">
+            <div class="mb-20">
+              <div class="d-flex justify-content-between mb-11">
+                <span
+                  class="badge font-lg-s font-xs bg-secondary text-black mb-4"
+                >
+                  藝術展覽
+                </span>
+                <ul class="d-flex">
+                  <li>
+                    <a href="#">
+                      <span class="me-2 text-info material-icons">
+                        bookmark_border
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span class="text-info material-icons"> share </span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="mb-15">
+                <h2 class="mb-6">胡桃夾子與他的王國</h2>
+                <p>香港芭蕾舞劇團</p>
+              </div>
+              <ul class="mb-15">
+                <li class="d-flex mb-6">
+                  <span class="text-primary material-icons font-m me-4">
+                    calendar_today
+                  </span>
+                  <ul>
+                    <li>2021-06-06 - 2021-07-07</li>
+                    <li>8:00 p.m</li>
+                  </ul>
+                </li>
+                <li class="d-flex mb-6">
+                  <span class="text-primary material-icons font-m me-4">
+                    location_on
+                  </span>
+                  <p>世紀廣場一號展覽廳</p>
+                </li>
+                <li class="d-flex mb-6">
+                  <span class="text-primary material-icons font-m me-4">
+                    paid
+                  </span>
+                  <p>$240 (學生票) / $480 (成人票)</p>
+                </li>
+              </ul>
+              <button class="btn btn-primary w-100 py-4">立即購票</button>
+            </div>
+            <div class="mb-15">
+              <p class="mb-8 fw-bold">節目介紹</p>
+              <p>
+                不以招地源天公；的又型、衣位中他名且分系象展不價、式看舉兒離時操、說持於公、中後以海：有見海感。思進說話怎子沒！如野果動眾。年建變如我深個千維支沒配大表起心多就長明致且數球感住有點草或家，完說長？認日用不起們傳計我停時火月路本內燈形生他散，來商萬金心當！何看因難知寶談助；是情是次來不里多便復親過文由多基育雜，其相如用二？西來許前。
+              </p>
+            </div>
+            <div class="text-info mb-15 font-s">
+              <p class="mb-8 fw-bold">入場須知</p>
+              <ol>
+                <li>請保持社交距離，以及佩戴口罩。</li>
+                <li>
+                  請於節目開始前十五分鐘進場，否則到中場休息時段才可進場。
+                </li>
+                <li>節目進行時，請關閉通訊設備鈴聲，以及保持安靜。</li>
+                <li>場內請勿飲食。</li>
+              </ol>
+            </div>
+            <div class="text-info font-s">
+              <p class="mb-8 fw-bold">購票須知</p>
+              <ol>
+                <li>購票交易成功後，將不能退款。</li>
+                <li>切勿轉售在此平台購買的票卷。</li>
+                <li>如購買學生票，需要在入場時向工作人員展示學生證件。</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Article -->
+      <div>
+        <h2 class="font-xl mb-14">獨家點評</h2>
+        <articleCard></articleCard>
+      </div>
+    </div>
+    <!-- Related events -->
+    <div class="bg-light py-18">
+      <div class="container">
+        <h2 class="font-xl mb-14">更多相關節目</h2>
+        <SwiperRelatedEvents></SwiperRelatedEvents>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,4 +129,12 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.main-image {
+  height: 674px;
+}
+
+.sub-image {
+  height: 320px;
+}
+</style>
