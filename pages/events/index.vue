3e495e49c216169e0631f923643ad5e7eea3ad80
alysachan830 <alysachan830@gmail.com>
2021-07-12 14:01:11 +0800

    Show all events in events page

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>default page</div>
+  <div>
+    <!-- Event cards -->
+    <client-only>
+      <div class="row mb-19">
+        <div
+          v-for="event in allEvents"
+          :key="event.id"
+          class="col-lg-3 col-md-4 col-6"
+        >
+          <EventCard
+            :title="event.title"
+            :image="event.imageUrl"
+            :date-time="event.dateTime"
+            :tag="event.tag"
+          ></EventCard>
+        </div>
+      </div>
+    </client-only>
+    <!-- Pagination -->
+    <div class="d-flex justify-content-center mb-19">
+      <Pagination></Pagination>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import EventCard from '@/components/user/EventCard.vue'
+import Pagination from '@/components/common/Pagination.vue'
+
+export default {
+  components: {
+    EventCard,
+    Pagination,
+  },
+  props: {
+    allEvents: {
+      type: Array,
+      required: true,
+    },
+  },
+}
 </script>
 
 <style></style>
