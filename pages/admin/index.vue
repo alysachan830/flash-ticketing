997163368ac537f4c4fbc70d0f2bf5dab67c05be
alysachan <alysachansl@gmail.com>
2021-07-19 21:43:06 +0800

    Show all events

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
+    <h2 class="font-xl mb-13">所有節目</h2>
     <table class="table">
       <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">節目標題</th>
+          <th scope="col">時間</th>
+          <th scope="col">價錢</th>
+          <th scope="col">狀態</th>
+          <th scope="col">編輯</th>
+          <th scope="col">刪除</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
+        <tr v-for="event in events" :key="event.id">
+          <td>{{ event.title }}</td>
+          <td>{{ dateTimeFormat(event.dateTime) }}</td>
+          <td>
+            <ul v-html="ticketPriceFormat(event.ticketPrice)"></ul>
+          </td>
+          <td>{{ event.is_enabled ? '已啟用' : '未啟用' }}</td>
+          <td>
+            <span class="material-icons"> edit </span>
+          </td>
+          <td>
+            <span class="material-icons"> clear </span>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -50,6 +50,26 @@ export default {
       // eslint-disable-next-line no-console
       console.log(error)
     }
+  },
+  methods: {
+    dateTimeFormat(dateTime) {
+      if (Array.isArray(dateTime)) {
+        return dateTime.map((item) => item.date).join(', ')
+      } else {
+        return `${dateTime.start} - ${dateTime.end}`
+      }
+    },
+    ticketPriceFormat(ticketPrice) {
+      if (typeof ticketPrice === 'object') {
+        // return Object.keys(ticketPrice).join(',')
+        return Object.keys(ticketPrice).reduce(
+          (acc, curr) => `${acc}<li>${curr}區：$${ticketPrice[curr]}</li>`,
+          ``
+        )
+      } else {
+        return `<li>${ticketPrice}</li>`
+      }
+    },
   },
 }
 </script>
